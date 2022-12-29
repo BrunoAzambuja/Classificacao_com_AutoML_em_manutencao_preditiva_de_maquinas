e4a1af6f5ba4b886b2e3c2632f04f2f2b06560fb
Bruno Azambuja <101824024+BrunoAzambuja@users.noreply.github.com>
2022-12-29 14:55:16 -0300

    Add files via upload

--- maintenance_app.py
+++ maintenance_app.py
@@ -10,31 +10,32 @@ st.title('Previsão de falhas em máquinas')
 st.write('##### App que utiliza machine learning para prever os tipos de falhas.')
 
 # input widgets
-airtemperature = st.number_input('Temperatura do ar [°C]:', min_value = 0.00, max_value = 45.00, step = 0.05)
 
-processtemperature = st.number_input('Temperatura do processo [°C]:', min_value = 0.00, max_value = 45.00, step = 0.05)
+Air_temperature = st.number_input('Temperatura do ar [°C]:', min_value = 0.00, max_value = 45.00, step = 0.05)
 
-rotationalspeed = st.number_input('Velocidade de rotação [rpm]:', min_value = 0, max_value = 3000, step = 1)
+Process_temperature = st.number_input('Temperatura do processo [°C]:', min_value = 0.00, max_value = 45.00, step = 0.05)
 
-torque = st.number_input('Torque [Nm]:', min_value = 0.0, max_value = 100.0, step = 0.1)
+Rotational_speed = st.number_input('Velocidade de rotação [rpm]:', min_value = 0, max_value = 3000, step = 1)
 
-tollwear = st.number_input('Tempo para o início do desgate [min]:', min_value = 0, max_value = 300, step = 1)
+Torque = st.number_input('Torque [Nm]:', min_value = 0.0, max_value = 100.0, step = 0.1)
+
+Toll_wear = st.number_input('Tempo para o início do desgate [min]:', min_value = 0, max_value = 300, step = 1)
 
 lista1 = ['0', '1']
-type1 = st.radio('Baixa qualidade do equipamento [0: não] [1: sim]:', options = lista1)
+Type_L = st.radio('Baixa qualidade do equipamento [0: não] [1: sim]:', options = lista1)
 
 lista2 = ['0', '1']
-type2 = st.radio('Média qualidade do equipamento [0: não] [1: sim]:', options = lista2)
+Type_M = st.radio('Média qualidade do equipamento [0: não] [1: sim]:', options = lista2)
 
 lista3 = ['0', '1']
-type3 = st.radio('Alta qualidade do equipamento [0: não] [1: sim]:', options = lista3)
+Type_H = st.radio('Alta qualidade do equipamento [0: não] [1: sim]:', options = lista3)
 
 
 if (os.path.exists('classifier_multiclass.pkl')):
     modelo = load('classifier_multiclass.pkl')
     botao = st.button('Previsão do tipo de falha [Multiclasse]')
     if(botao):
-        listaValores = np.array([[airtemperature, processtemperature, rotationalspeed, torque, tollwear, type1, type2, type3]])
+        listaValores = np.array([[Air_temperature, Process_temperature, Rotational_speed, Torque, Toll_wear, Type_L, Type_M, Type_H]], dtype=float)
         resultado = modelo.predict(listaValores)
 
         if(resultado[0] == 'Random Failures'):
@@ -56,4 +57,6 @@ if (os.path.exists('classifier_multiclass.pkl')):
 
 else:
     st.error('Erro ao carregar o modelo preditivo. Contacte o administrador do sistema.')
+
+
 
